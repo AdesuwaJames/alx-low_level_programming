a8301d240fd13605ffda0ee9eecef57a425f7122
Boluwatife <eseofemoyaki@gmail.com>
2022-06-02 14:08:23 -0700

    task5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ *  main - entry point
+ *  Return: it is going to always return 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+}
